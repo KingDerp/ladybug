b5443ab9a2fc0da5b5940d911ec1002b1678dc32
mac <mac.farnsworth@live.com>
2018-05-17 21:03:41 -0600

    bare bones authentication with session

--- cmd/ladybug/main.go
+++ cmd/ladybug/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"ladybug/handlers"
 	"net/http"
+	"os"
+
+	"github.com/zeebo/errs"
 )
 
 func main() {
+	err := run(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%+v\n", err)
+	}
 }
 
 func run(ctx context.Context) error {
 	handler := handlers.NewHandler()
 
-	return http.ListenAndServe("localhost:8080", handler)
+	return errs.Wrap(http.ListenAndServe("localhost:8080", handler))
 }
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -11,15 +11,33 @@ type Handler struct {
 
 func NewHandler() *Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		// The "/" pattern matches everything, so we need to check
-		// that we're at the root here.
-		if req.URL.Path != "/" {
-			http.NotFound(w, req)
-			return
-		}
-		fmt.Fprintf(w, "Welcome to the home page!")
-	})
+	mux.Handle("/",
+		Wrap(
+			http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				// The "/" pattern matches everything, so we need to check
+				// that we're at the root here.
+				if req.URL.Path != "/" {
+					http.NotFound(w, req)
+					return
+				}
+				fmt.Fprintf(w, "Welcome to the home page!")
+			}),
+		),
+	)
 
 	return &Handler{Handler: mux}
+}
+
+func Wrap(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		cookie, err := r.Cookie("session")
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		cookie = cookie
+
+		handler.ServeHTTP(w, r)
+	})
 }
